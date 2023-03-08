b74b8ce79c211c6e104e1a39eb2e77cb806fe342
mohamedisakr <mohamedisakr@gmail.com>
2023-03-08 09:57:54 +0200

    coercion rules

--- building-blocks.R
+++ building-blocks.R
@@ -70,6 +70,15 @@ course_toc <- "The course is consists of 12 sections"
 typeof(course_toc)
 
 
+
+course_name <- c("R", "101")
+
+print(typeof(course_name))
+
+print(course_name)
+
+
+
 heights <-  c(11.5200, 12.4800, 12.3778, 12.7300, 12.4440, 13.6024)
 
 print(heights)
@@ -77,47 +86,22 @@ print(heights)
 print(typeof(heights))
 
 
-
-at_least_heights <- heights>12
-
-(typeof(at_least_heights))
-
-
-
-# x <- c(2, 3, 5, 7, 11)
-# 
-# print(typeof(x))
-# 
-# print(x)
-
-
-ls()
-
 height_above = heights > 12
 
 print(height_above)
 
 print(typeof(height_above))
 
-
-
 ls()
 
 
+#----------------------- coercion rules -------------------
 
-
-course_name <- c("R", "101")
-
-
-print(typeof(course_name))
-
-
-print(course_name)
-
-
-#check if my_data exists
-#exists('course_name')
-
+# x <- c(2, 3, 5, 7, 11)
+# 
+# print(typeof(x))
+# 
+# print(x)
 
 mixed <- c("characters", TRUE, 1)
 
@@ -126,6 +110,14 @@ mixed <- c("characters", TRUE, 1)
 print(typeof(mixed))
 
 print(a)
+
+ls()
+
+
+
+#check if my_data exists
+#exists('course_name')
+
 
 mean(a)
 
