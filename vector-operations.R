36a14a2dac5764ea0399f9bed986a3614a9ac9a6
mohamedisakr <mohamedisakr@gmail.com>
2023-03-08 14:06:08 +0200

    vector slicing & indexing

--- vector-operations.R
+++ vector-operations.R
@@ -1,4 +1,4 @@
-#------------------ 1. Introduction to vectors ----------------
+#------------------ 1. introduction to vectors ----------------
 vec <- c(1,2,3)
 vic <- c(11,12,13)
 
@@ -23,7 +23,9 @@ prod(vic)
 min(vic)
 max(vic)
 
-#------------------ 2. Vector recycling ----------------
+
+
+#------------------ 2. vector recycling ----------------
 long_vec <- 1:10
 short_vec <- 1:5
 
@@ -32,7 +34,9 @@ long_vec - short_vec
 long_vec / short_vec
 long_vec * short_vec
 
-#------------------ 3. Vector attributes  ----------------
+
+
+#------------------ 3. vector attributes  ----------------
 # names, dimensions, classes
 
 age <- c(23, 26, 24, 26)
@@ -54,27 +58,39 @@ names(age) <- NULL
 names(age)
 
 
-# ----------------- Slicing and indexing a vector -------------------
+
+# ----------------- 4. slicing & indexing a vector -------------------
 
 n.deck<-c(6,7,8,9,10)
 
 deck <- c("Duke", "Assassin", "Captain", "Ambassador", "Contessa")
 
+# select the 4th element
 deck[4]
 
+# select all but not the 4th element
 deck[-4]
+
 
 deck[c(1,3,5)]
 
-names(n.deck) <- deck
+odd_deck <- n.deck[n.deck %% 2 == 1]
+odd_deck
 
+
+names(n.deck) <- deck
 n.deck
+
 
 n.deck["Contessa"]
 
 n.deck[c("Contessa", "Duke", "Captain")]
 
+# slicing
+
 n.deck[1:2]
+
+n.deck[3:5]
 
 lv <- seq(10, 100, by=10)
 
@@ -82,6 +98,7 @@ lv[-(4:7)]
 
 lv[lv > 30]
 
+lv[lv %% 2 == 0]
 
 # ----------------------  Changing the dimensions of an object -----------------
 
