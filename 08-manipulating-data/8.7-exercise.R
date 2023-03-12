b9e3e65f5e18936ea9351026d77bceb413dfe17e
mohamedisakr <mohamedisakr@gmail.com>
2023-03-12 11:09:49 +0200

    exercise 7

--- 08-manipulating-data/8.7-exercise.R
+++ 08-manipulating-data/8.7-exercise.R
@@ -2,11 +2,35 @@
 # Please find a lot less tidy version in the resources for this lesson and go through the 
 # necessary steps to clean it.
 
-weather_data = read.csv("weather-untidy.csv", stringsAsFactors = FALSE)
-str(weather_data)
+library(tidyverse)
 
+weather_data <- read.csv("weather-untidy.csv", stringsAsFactors = FALSE)
+weather_data <- as.tibble(weather_data)
+str(weather_data)
+head(weather_data)
+
+colnames(weather_data)
+ncol(weather_data)
+
+# weather_group_by_year <- weather_data %>%
+# weather_filtered <- weather_data %>% group_by(element) %>% summarize(count = n())
+# weather_filtered
+
+weather_gathered <- 
+  weather_data %>% 
+  gather(d1:d31, key = "day", value = "temperature", na.rm = T) %>%
+  arrange(year, month)
+weather_gathered
+        
 # Note: read the documentation on the parse_number() function from the readr package, and 
 # try to use it when cleaning the data.
+
+weather_spread <- weather_gathered %>%
+  spread(key =  element, value =  temperature) %>%
+  arrange(year, month)
+
+weather_spread
+
 
 # Play around with the tb data, too; try to reproduce what we did in the lesson - you will 
 # need to do a little bit more tidying than we did together. Hint: if you are confused about 
