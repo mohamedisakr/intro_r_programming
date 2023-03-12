fc1ee84472bb7a92fc4846b79843ec2afae2084f
mohamedisakr <mohamedisakr@gmail.com>
2023-03-12 19:03:00 +0200

    building histogram

--- 09-visualizing-data/visualizing-data.R
+++ 09-visualizing-data/visualizing-data.R
@@ -26,6 +26,38 @@ scatterplot + geom_point(aes(color = Region), size = 3)
 scatterplot + geom_point(aes(color = Region), size = 3) + stat_smooth() + theme_minimal()
 
 
+#----------------- building histogram ----------------
+
+
+library(tidyverse)
+
+
+df <- read.csv("titanic.csv", stringsAsFactors = FALSE)
+df <- as.tibble(df)
+
+df
+
+
+df$Survived <- as.factor(df$Survived)
+df$Pclass <- as.factor(df$Pclass)
+df$Sex <- as.factor(df$Sex)
+df$Embarked <- as.factor(df$Embarked)
+
+str(df)
+
+hist <- ggplot(data = df, aes(x = Age))
+
+hist + geom_histogram(binwidth = 5, color = "darkslategray", fill = "darkslategray4", alpha = 0.5) + 
+  ggtitle("Age Distribution") + labs(y = "# Passangers", x = "Age") + theme_minimal()
+
+
+
+
+
+
+
+
+
 
 
 
