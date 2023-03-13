4564ad48c445394a196183e1cfeccc590a278fdc
mohamedisakr <mohamedisakr@gmail.com>
2023-03-13 14:53:07 +0200

    multivariate measures of dispersion

--- 10-exploratory-data-analysis/exploratory-data-analysis.R
+++ 10-exploratory-data-analysis/exploratory-data-analysis.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# ---------------- Variance & standard deviation & coefficient of variability ---------------------
+# ---------------- variance & standard deviation & coefficient of variability ---------------------
 # univariate measures of dispersion
 
 ny <- c(1, 2, 3, 3, 5, 6, 7, 8, 9, 11)
@@ -17,4 +17,26 @@ sapply(pizza, var)
 
 coef_var <- sapply(pizza, sd)/sapply(pizza, mean)
 coef_var
+
+
+# ---------------- covariance & correlation ---------------------
+# multivariate measures of dispersion
+
+land_states <- read.csv("landdata-states.csv", stringsAsFactors = FALSE)
+land_states <- as_tibble(land_states)
+str(land_states)
+land_states
+
+land_states %>% subset(Date == 2001.25) %>% 
+  ggplot(aes(y = Structure.Cost, x = log(Land.Value))) + 
+  geom_point() + theme_light() + 
+  labs(x = "Land Value (transformed)", y = "Structure Cost (USD)", 
+       title = "Relationship between land value and structure cost")
+
+cor_coef <- cor(land_states$Structure.Cost, land_states$Land.Value)
+cor_coef
+
+cor_coef_test <- cor.test(land_states$Structure.Cost, land_states$Land.Value)
+cor_coef_test
+
 
