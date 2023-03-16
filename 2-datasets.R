ffe25b1685d037809865e30874505889cace7a47
mohamedisakr <mohamedisakr@gmail.com>
2023-03-16 08:59:47 +0200

    changing file location

--- 2-datasets.R
+++ 2-datasets.R
@@ -1,11 +1,22 @@
 library(tidyverse)
 
-summer_games <- read.csv("summer-olympic-games.csv", stringsAsFactors = FALSE)
+# define paths to data sets
+directory_path <- file.path(dirname(dirname(getwd())))
+directory_path
+
+summer_file_path <- file.path(directory_path, "data", "summer-olympic-games.csv")
+summer_file_path
+
+winter_file_path <- file.path(directory_path, "data", "winter-olympic-games.csv")
+winter_file_path
+
+# import the data sets
+summer_games <- read.csv(summer_file_path, stringsAsFactors = FALSE)
 summer_games <- as_tibble(summer_games)
 summer_games
 str(summer_games)
 
-winter_games <- read.csv("winter-olympic-games.csv", stringsAsFactors = FALSE)
+winter_games <- read.csv(winter_file_path, stringsAsFactors = FALSE)
 winter_games <- as_tibble(winter_games)
 winter_games
 str(winter_games)
