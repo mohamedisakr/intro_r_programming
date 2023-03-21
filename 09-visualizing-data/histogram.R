f38a39eabf1beb3767b51b606bfefef9fb089fd5
mohamedisakr <mohamedisakr@gmail.com>
2023-03-21 09:42:22 +0200

    histogram with ggplot

--- 09-visualizing-data/histogram.R
+++ 09-visualizing-data/histogram.R
@@ -17,7 +17,7 @@ library(tidyverse)
 # colnames(summer_games)
 
 
-# ------------------- without using ggplot ------------------
+# ------------------- basic formatting ------------------
 hist(iris$Petal.Length,
      main="Distribution of Petal Length",
      xlab="Petal Length",
@@ -25,7 +25,48 @@ hist(iris$Petal.Length,
 
 
 
-# ------------------- using ggplot ------------------
+# ------------------- density instead of frequency ------------------
+hist(iris$Petal.Length,
+     main="Distribution of Petal Length",
+     xlab="Petal Length",
+     col="steelblue",
+     freq=FALSE)
 
 
+# --------------- density instead of frequency (adding a kernel density line) ----------------
+lines(density(iris$Petal.Length))
+
+
+# --------------- adding a rug ----------------
+rug(iris$Petal.Length)
+
+
+# --------------- relative frequency (more advanced) ----------------
+h <- hist(iris$Petal.Length, plot=FALSE)
+h$density = h$counts/sum(h$counts) * 100
+plot(h, main="Distribution of Petal Length",
+     xlab="Lengh ($K)",
+     ylab="Percent",
+     col="orangered",
+     freq=FALSE)
+
+
+# --------------- using ggplot ----------------
+histo <- ggplot(data=iris) 
+histo +  geom_histogram(mapping = aes(x = Petal.Length), bins = 10, fill = "steelblue") +
+  ggtitle("Distribution of Petal Length") + xlab("Length")
+
+
+# ----------------------------- Note ------------------------------------------------------
+# mapping=aes(x=Salary, y=..count..)  ## no change: y=..count.. is default`
+# mapping=aes(x=Salary, y=..density..)  ## shows density, as above in hist()`
+# mapping=aes(x=Salary, y=..count../sum(..count..)*100)  ## shows relative frequency`
+# ----------------------------------------------------------------------------------------------
+
+# --------------- using ggplot - relative frequency ----------------
+histo <- ggplot(data=iris) 
+histo +  
+  geom_histogram(mapping=aes(x=Petal.Length, y=after_stat(count)/sum(after_stat(count)*100), 
+                 bins=10, fill = "steelblue")) + 
+  ggtitle("Distribution of Petal Length") + xlab("Length")
 
